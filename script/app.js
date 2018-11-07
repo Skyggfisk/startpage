14dc89e8d18680076cacb3e32bb4a85dc384aba4
Skyggfisk <mahasi93@hotmail.com>
2018-11-07 06:20:05 +0100

    all in a days work

--- script/app.js
+++ script/app.js
@@ -11,6 +11,9 @@ initGreetings();
 initClock();
 initTitle();
 initWeather();
+initRss();
+initQuote();
+initBookmarks();
 
 function initTitle() {
   var r = Math.round(Math.random() * (titles.length - 1));
@@ -22,6 +25,53 @@ function initGreetings() {
   $(".greetings .greetings-name").html(user);
 }
 
+function initRss() {
+  $(feeds).each(function(index, feed) {
+    $("#rss-card").append(`<p class="rss-title">${feed[0]}</p>`);
+    $("#rss-card").append("<div id='" + index + "'></div");
+    $("#rss-card #" + index).FeedEk({
+      FeedUrl: feed[1],
+      MaxCount: 5,
+      ShowDesc: false,
+      DateFormat: "DD.MM.YYYY",
+      DateFormatLang: "en"
+    });
+  });
+}
+
+function initQuote() {
+  var r = Math.round(Math.random() * (quotes.length - 1));
+  var q = quotes[r];
+  $(".quote-card").append(`<p class="quote-text">"${q[0]}"</p>`);
+  $(".quote-card").append(`<p class="quote-author">-${q[1]}</p>`);
+}
+
+function initBookmarks() {
+  var element = "";
+  $(favorites).each(function(index, group) {
+    element +=
+      "" + '<div class="favorite">' + '<p class="title">' + group[0] + "</p>";
+    "<ul>" +
+      $(group[1]).each(function(index, favorite) {
+        element +=
+          "<li>" +
+          "<span class='short'><a target='_blank' href='" +
+          favorite[1] +
+          "'>" +
+          favorite[2] +
+          "</a></span>" +
+          "<span class='link'><a target='_blank' href='" +
+          favorite[1] +
+          "'>" +
+          favorite[0] +
+          "</a></span>" +
+          "</li>";
+      });
+    element += "" + "</ul>" + "</div>";
+  });
+  $("#bookmarks-card").append(element);
+}
+
 function initWeather() {
   $.simpleWeather({
     zipcode: "",
@@ -30,9 +80,9 @@ function initWeather() {
     unit: "c",
     success: function(weather) {
       $(".weather-location").html(`${weather.city}, ${weather.region}`);
-      $(".weather-icon").html(setWeatherIcon(weather.code));
+      $(".weather-icon").html(`<i class="wi wi-yahoo-${weather.code}"></i>`);
       $(".weather-temperature").html(
-        `${weather.temp} &deg ${weather.units.temp}`
+        `${weather.temp}&deg${weather.units.temp}`
       );
       $(".weather-description").html(`${weather.currently}`);
     },
@@ -57,12 +107,8 @@ function initClock() {
   if (m < 10) m = "0" + m;
   if (s < 10) s = "0" + s;
 
-  $(".time-hours").html(h);
-  $(".time-minutes").html(m);
-  $(".time-seconds").html(s);
-  $(".date-day").html(dd);
-  $(".date-month").html(mm);
-  $(".date-year").html(yyyy);
+  $(".time-hours").html(`${h}:${m}:${s}`);
+  $(".date-day").html(`${dd}.${mm}.${yyyy}`);
 
   if (h < 12) {
     $(".greetings-title").html("good morning");
@@ -75,165 +121,6 @@ function initClock() {
   var t = setTimeout(initClock, 500);
 }
 
-function setWeatherIcon(condid) {
-  var icon = "";
-  switch (condid) {
-    case "0":
-      icon = "wi-tornado";
-      break;
-    case "1":
-      icon = "wi-storm-showers";
-      break;
-    case "2":
-      icon = "wi-tornado";
-      break;
-    case "3":
-      icon = "wi-thunderstorm";
-      break;
-    case "4":
-      icon = "wi-thunderstorm";
-      break;
-    case "5":
-      icon = "wi-snow";
-      break;
-    case "6":
-      icon = "wi-rain-mix";
-      break;
-    case "7":
-      icon = "wi-rain-mix";
-      break;
-    case "8":
-      icon = "wi-sprinkle";
-      break;
-    case "9":
-      icon = "wi-sprinkle";
-      break;
-    case "10":
-      icon = "wi-hail";
-      break;
-    case "11":
-      icon = "wi-showers";
-      break;
-    case "12":
-      icon = "wi-showers";
-      break;
-    case "13":
-      icon = "wi-snow";
-      break;
-    case "14":
-      icon = "wi-storm-showers";
-      break;
-    case "15":
-      icon = "wi-snow";
-      break;
-    case "16":
-      icon = "wi-snow";
-      break;
-    case "17":
-      icon = "wi-hail";
-      break;
-    case "18":
-      icon = "wi-hail";
-      break;
-    case "19":
-      icon = "wi-cloudy-gusts";
-      break;
-    case "20":
-      icon = "wi-fog";
-      break;
-    case "21":
-      icon = "wi-fog";
-      break;
-    case "22":
-      icon = "wi-fog";
-      break;
-    case "23":
-      icon = "wi-cloudy-gusts";
-      break;
-    case "24":
-      icon = "wi-cloudy-windy";
-      break;
-    case "25":
-      icon = "wi-thermometer";
-      break;
-    case "26":
-      icon = "wi-cloudy";
-      break;
-    case "27":
-      icon = "wi-night-cloudy";
-      break;
-    case "28":
-      icon = "wi-day-cloudy";
-      break;
-    case "29":
-      icon = "wi-night-cloudy";
-      break;
-    case "30":
-      icon = "wi-day-cloudy";
-      break;
-    case "31":
-      icon = "wi-night-clear";
-      break;
-    case "32":
-      icon = "wi-day-sunny";
-      break;
-    case "33":
-      icon = "wi-night-clear";
-      break;
-    case "34":
-      icon = "wi-day-sunny-overcast";
-      break;
-    case "35":
-      icon = "wi-hail";
-      break;
-    case "36":
-      icon = "wi-day-sunny";
-      break;
-    case "37":
-      icon = "wi-thunderstorm";
-      break;
-    case "38":
-      icon = "wi-thunderstorm";
-      break;
-    case "39":
-      icon = "wi-thunderstorm";
-      break;
-    case "40":
-      icon = "wi-storm-showers";
-      break;
-    case "41":
-      icon = "wi-snow";
-      break;
-    case "42":
-      icon = "wi-snow";
-      break;
-    case "43":
-      icon = "wi-snow";
-      break;
-    case "44":
-      icon = "wi-cloudy";
-      break;
-    case "45":
-      icon = "wi-lightning";
-      break;
-    case "46":
-      icon = "wi-snow";
-      break;
-    case "47":
-      icon = "wi-thunderstorm";
-      break;
-    case "3200":
-      icon = "wi-cloud";
-      break;
-    default:
-      icon = "wi-cloud";
-      break;
-  }
-
-  return '<i class="wi ' + icon + '"></i>';
-}
-
-// On document ready run our functions
 $(document).ready(function() {
   init();
 });
--- script/var.js
+++ script/var.js
@@ -1,6 +1,16 @@
 var user = "skyggfisk";
+
 var locations = ["551890"];
+
 var titles = ["Soothing shadows", "Oathbound", "Grab a coffee"];
+
+var quotes = [
+  [
+    "Tradition is not the worship of ashes, but the preservation of fire.",
+    "Gustav Mahler"
+  ]
+];
+
 var images = [
   "1412446496031.jpg",
   "1417804954510.jpg",
@@ -23,4 +33,61 @@ var images = [
   "1448399280112.jpg",
   "1450066383293.jpg",
   "1450074394745.jpg"
+];
+var feeds = [
+  ["Hacker news", "https://news.ycombinator.com/rss"],
+  ["Version2", "https://www.version2.dk/it-nyheder/rss"]
+];
+
+var favorites = [
+  [
+    "Work",
+    [
+      ["gmail", "https://mail.google.com/mail/u/0/#inbox", "gm"],
+      ["hotmail", "https://login.live.com/", "hm"],
+      ["github", "https://github.com/", "gh"],
+      ["bitbucket", "http://bitbucket.org", "bb"],
+      ["calendar", "https://calendar.google.com/calendar/r", "gc"]
+    ]
+  ],
+  [
+    "Social",
+    [
+      ["facebook", "https://www.facebook.com/", "fb"],
+      ["twitter", "https://twitter.com/", "twi"]
+    ]
+  ],
+  [
+    "Download",
+    [
+      ["majorgeeks", "https://www.majorgeeks.com/", "mg"],
+      ["T411", "http://www.t411.in/", "t411"]
+    ]
+  ],
+  [
+    "Reddit",
+    [
+      ["frontpage", "https://www.reddit.com/", "fp"],
+      ["world of warcraft", "https://www.reddit.com/r/wow/", "wow"],
+      ["4chan", "https://www.reddit.com/r/4chan/", "4ch"],
+      ["world news", "https://www.reddit.com/r/worldnews/", "wn"],
+      ["programmer humor", "https://www.reddit.com/r/ProgrammerHumor/", "ph"]
+    ]
+  ],
+  [
+    "4chan",
+    [
+      ["home", "http://4chan.org/", "/"],
+      ["wallpaper", "http://4chan.org/wg/catalog", "/wg/"],
+      ["technology", "http://4chan.org/g/catalog", "/g/"]
+    ]
+  ],
+  [
+    "Others",
+    [
+      ["hugelol", "http://hugelol.com", "hgl"],
+      ["hiddenlol", "http://hiddenlol.com", "hdl"],
+      ["imgur", "https://imgur.com/", "img"]
+    ]
+  ]
 ];
