d799521e918260cfa1f5c55cabbe1b512ff559a9
Skyggfisk <mahasi93@hotmail.com>
2022-04-21 20:07:16 +0200

    use double quoted strings, start getting rid of backstretchjs

--- script/app.js
+++ script/app.js
@@ -1,9 +1,9 @@
 function init() {
   // Sets background image using backstretch
-  $('body').backstretch(
-    `images/backgrounds/${Math.floor(Math.random() * 44)}.jpg`,
-    { fade: 300 }
-  );
+  // $("body").backstretch(
+  //   `images/backgrounds/${Math.floor(Math.random() * 44)}.jpg`,
+  //   { fade: 300 }
+  // );
 
   // init all the things
   initTitle();
@@ -18,26 +18,27 @@ function init() {
 // Grab a random title and set it
 function initTitle() {
   var r = Math.round(Math.random() * (titles.length - 1));
-  $('title').html(titles[r]);
-  $('#logo-text').html(titles[r]);
+  $("title").html(titles[r]);
+  // $("#logo-text").html(titles[r]);
 }
 
 // Greet the user
 function initGreetings() {
-  $('.greetings .greetings-name').html(user);
+  $(".greetings .greetings-name").html(user);
 }
 
 // Create a feed for each in var.js
 // TODO get for each feed in var
+// TODO: cache feed in localStorage
 function initRss() {
-  $('#rss-card').append(`<p class='rss-title'>${feeds[0][0]}</p>`);
-  $('#rss-card').append(`<div id='0'></div>`);
-  feednami.load(feeds[0][1]).then(feed => {
-    console.log(feed);
-    $('#rss-card #0').append('<ul class="feedEkList" id="0"></ul>');
+  $("#rss-card").append(`<p class='rss-title'>${feeds[0][0]}</p>`);
+  $("#rss-card").append(`<div id='0'></div>`);
+  feednami.load(feeds[0][1]).then((feed) => {
+    // console.log(feed);
+    $("#rss-card #0").append('<ul class="feedEkList" id="0"></ul>');
     for (let entry of feed.entries) {
-      let dt = moment(entry.pubDate).format('DD-MM-YYYY');
-      $('#rss-card #0 #0').append(
+      let dt = moment(entry.pubDate).format("DD-MM-YYYY");
+      $("#rss-card #0 #0").append(
         `<li>
           <div class='itemTitle'>
             <a href='${entry.link}' target='_blank'>${entry.title}</a>
@@ -49,7 +50,7 @@ function initRss() {
   });
 
   if ($(window).width() > 768) {
-    $('#rss-card').slimscroll({ height: '800px' });
+    $("#rss-card").slimscroll({ height: "800px" });
   }
 }
 
@@ -57,55 +58,55 @@ function initRss() {
 function initQuote() {
   var r = Math.round(Math.random() * (quotes.length - 1));
   var q = quotes[r];
-  $('.quote-card').append(`<p class="quote-text">"${q[0]}"</p>`);
-  $('.quote-card').append(`<p class="quote-author">-${q[1]}</p>`);
+  $(".quote-card").append(`<p class="quote-text">"${q[0]}"</p>`);
+  $(".quote-card").append(`<p class="quote-author">-${q[1]}</p>`);
 }
 
 // Create a favorite div for each in var.js and display
 function initBookmarks() {
-  var element = '';
-  $(favorites).each(function(index, group) {
+  var element = "";
+  $(favorites).each(function (index, group) {
     element +=
-      '' + '<div class="favorite">' + '<p class="title">' + group[0] + '</p>';
-    '<ul>' +
-      $(group[1]).each(function(index, favorite) {
+      "" + '<div class="favorite">' + '<p class="title">' + group[0] + "</p>";
+    "<ul>" +
+      $(group[1]).each(function (index, favorite) {
         element +=
-          '<li>' +
+          "<li>" +
           "<span class='short'><a target='_blank' href='" +
           favorite[1] +
           "'>" +
           favorite[2] +
-          '</a></span>' +
+          "</a></span>" +
           "<span class='link'><a target='_blank' href='" +
           favorite[1] +
           "'>" +
           favorite[0] +
-          '</a></span>' +
-          '</li>';
+          "</a></span>" +
+          "</li>";
       });
-    element += '' + '</ul>' + '</div>';
+    element += "" + "</ul>" + "</div>";
   });
-  $('#bookmarks-card').append(element);
+  $("#bookmarks-card").append(element);
 }
 
 // jQuery simpleWeather and display it on success, else display error
 function initWeather() {
   $.simpleWeather({
-    zipcode: '',
+    zipcode: "",
     woeid: locations[0],
-    location: '',
-    unit: 'c',
-    success: function(weather) {
-      $('.weather-location').html(`${weather.city}, ${weather.region}`);
-      $('.weather-icon').html(`<i class="wi wi-yahoo-${weather.code}"></i>`);
-      $('.weather-temperature').html(
+    location: "",
+    unit: "c",
+    success: function (weather) {
+      $(".weather-location").html(`${weather.city}, ${weather.region}`);
+      $(".weather-icon").html(`<i class="wi wi-yahoo-${weather.code}"></i>`);
+      $(".weather-temperature").html(
         `${weather.temp}&deg${weather.units.temp}`
       );
-      $('.weather-description').html(`${weather.currently}`);
+      $(".weather-description").html(`${weather.currently}`);
     },
-    error: function(error) {
-      $('.weather-location').html(`<p>${error}</p>`);
-    }
+    error: function (error) {
+      $(".weather-location").html(`<p>${error}</p>`);
+    },
   });
 }
 
@@ -118,27 +119,31 @@ function initClock() {
   var dd = today.getDate();
   var mm = today.getMonth() + 1;
   var yyyy = today.getFullYear();
+  const timeOfDay =
+    h < 12 ? "morning" : h >= 12 && h < 19 ? "afternoon" : "evening";
 
-  if (dd < 10) dd = '0' + dd;
-  if (mm < 10) mm = '0' + mm;
-  if (h < 10) h = '0' + h;
-  if (m < 10) m = '0' + m;
-  if (s < 10) s = '0' + s;
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  if (h < 10) h = "0" + h;
+  if (m < 10) m = "0" + m;
+  if (s < 10) s = "0" + s;
 
-  $('.time-hours').html(`${h}:${m}:${s}`);
-  $('.date-day').html(`${dd}.${mm}.${yyyy}`);
+  $(".time-hours").html(`${h}:${m}:${s}`);
+  $(".date-day").html(`${dd}.${mm}.${yyyy}`);
 
-  if (h < 12) {
-    $('.greetings-title').html('good morning');
-  } else if (h >= 12 && h < 19) {
-    $('.greetings-title').html('good afternoon');
-  } else {
-    $('.greetings-title').html('good evening');
-  }
+  // if (h < 12) {
+  //   $(".greetings-title").html("good morning");
+  // } else if (h >= 12 && h < 19) {
+  //   $(".greetings-title").html("good afternoon");
+  // } else {
+  //   $(".greetings-title").html("good evening");
+  // }
+
+  $(".greetings-title").html(`Good ${timeOfDay},`);
 
   var t = setTimeout(initClock, 500);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   init();
 });
--- script/var.js
+++ script/var.js
@@ -1,97 +1,99 @@
-const user = 'skyggfisk';
+const user = "Skyggfisk";
 
-const locations = ['551890'];
+const locations = ["551890"];
 
 const titles = [
-  'Soothing shadows',
-  'Oathbound',
-  'Grab a cuppa',
-  'Go for a run',
-  'Hey you, check your posture',
-  'Give me 10 push-ups!',
-  'The ride never ends.'
+  "Soothing shadows",
+  "Oathbound",
+  "Grab a cuppa",
+  "Go for a run",
+  "Hey you, check your posture",
+  "Give me 10 push-ups!",
+  "The ride never ends.",
 ];
 
+// TODO: replace with norse sayings and mythology
 const quotes = [
   [
-    'Tradition is not the worship of ashes, but the preservation of fire.',
-    'Gustav Mahler'
+    "Tradition is not the worship of ashes, but the preservation of fire.",
+    "Gustav Mahler",
   ],
   [
-    'Man cannot remake himself without suffering, for he is both the marble and the sculptor',
-    'Alexis Carrel'
+    "Man cannot remake himself without suffering, for he is both the marble and the sculptor",
+    "Alexis Carrel",
   ],
-  ['Only the dead have seen the end of war.', 'Plato'],
-  ['It is no bad thing celebrating a simple life.', 'J. R. R. Tolkien'],
+  ["Only the dead have seen the end of war.", "Plato"],
+  ["It is no bad thing celebrating a simple life.", "J. R. R. Tolkien"],
   [
-    'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.',
-    'Nikola Tesla'
+    "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.",
+    "Nikola Tesla",
   ],
   [
-    'What is really needed is not to toss back and forth in a bed of agony, but to awaken and get up.',
-    'Julius Evola'
-  ]
+    "What is really needed is not to toss back and forth in a bed of agony, but to awaken and get up.",
+    "Julius Evola",
+  ],
 ];
 
 const feeds = [
-  ['Hacker news', 'https://news.ycombinator.com/rss'],
-  ['Version2', 'https://www.version2.dk/it-nyheder/rss'],
-  ['tv2', 'https://feeds.tv2.dk/nyhederne_seneste/rss']
+  ["Hacker news", "https://news.ycombinator.com/rss"],
+  ["Version2", "https://www.version2.dk/it-nyheder/rss"],
+  ["tv2", "https://feeds.tv2.dk/nyhederne_seneste/rss"],
 ];
 
+// TODO: clean up this data structure - json is a better fit
 const favorites = [
   [
-    'Work',
+    "Work",
     [
-      ['gmail', 'https://mail.google.com/mail/u/0/#inbox', 'gm'],
-      ['hotmail', 'https://login.live.com/', 'hm'],
-      ['github', 'https://github.com/', 'gh'],
-      ['bitbucket', 'http://bitbucket.org', 'bb'],
-      ['calendar', 'https://calendar.google.com/calendar/r', 'gc']
-    ]
+      ["gmail", "https://mail.google.com/mail/u/0/#inbox", "gm"],
+      ["hotmail", "https://login.live.com/", "hm"],
+      ["github", "https://github.com/", "gh"],
+      ["bitbucket", "http://bitbucket.org", "bb"],
+      ["calendar", "https://calendar.google.com/calendar/r", "gc"],
+    ],
   ],
   [
-    'Social',
+    "Social",
     [
-      ['facebook', 'https://www.facebook.com/', 'fb'],
-      ['youtube', 'https://www.youtube.com/', 'yt'],
-      ['twitch', 'https://www.twitch.tv/', 'ttv'],
-      ['twitter', 'https://twitter.com/', 'twi']
-    ]
+      ["facebook", "https://www.facebook.com/", "fb"],
+      ["youtube", "https://www.youtube.com/", "yt"],
+      ["twitch", "https://www.twitch.tv/", "ttv"],
+      ["twitter", "https://twitter.com/", "twi"],
+    ],
   ],
   [
-    'Music',
+    "Music",
     [
-      ['bandcamp', 'https://www.bandcamp.com/', 'bc'],
-      ['soundcloud', 'http://www.https://soundcloud.com/', 'sc'],
-      ['spotify', 'https://www.spotify.com/', 'spf']
-    ]
+      ["bandcamp", "https://www.bandcamp.com/", "bc"],
+      ["soundcloud", "http://www.https://soundcloud.com/", "sc"],
+      ["spotify", "https://www.spotify.com/", "spf"],
+    ],
   ],
   [
-    'Reddit',
+    "Reddit",
     [
-      ['frontpage', 'https://www.reddit.com/', 'fp'],
-      ['Denmark', 'https://www.reddit.com/r/Denmark/', 'dk'],
-      ['world news', 'https://www.reddit.com/r/worldnews/', 'wn'],
-      ['programmer humor', 'https://www.reddit.com/r/ProgrammerHumor/', 'ph']
-    ]
+      ["frontpage", "https://www.reddit.com/", "fp"],
+      ["Denmark", "https://www.reddit.com/r/Denmark/", "dk"],
+      ["world news", "https://www.reddit.com/r/worldnews/", "wn"],
+      ["programmer humor", "https://www.reddit.com/r/ProgrammerHumor/", "ph"],
+    ],
   ],
   [
-    '4chan',
+    "4chan",
     [
-      ['tech', 'http://boards.4channel.org/g/', '/g/'],
-      ['games', 'http://boards.4channel.org/v/catalog/', '/v/'],
-      ['gifs', 'http://boards.4channel.org/wsg/', '/wsg/'],
-      ['business', 'http://boards.4channel.org/biz/catalog', '/biz/']
-    ]
+      ["tech", "http://boards.4channel.org/g/", "/g/"],
+      ["games", "http://boards.4channel.org/v/catalog/", "/v/"],
+      ["gifs", "http://boards.4channel.org/wsg/", "/wsg/"],
+      ["business", "http://boards.4channel.org/biz/catalog", "/biz/"],
+    ],
   ],
   [
-    'Other',
+    "Other",
     [
-      ['amazon', 'https://www.amazon.com/', 'amz'],
-      ['graphic', 'https://graphicburger.com/', 'gb'],
-      ['gutenberg', 'https://www.gutenberg.org/', 'pg'],
-      ['rainy mood', 'https://rainymood.com/', 'rm']
-    ]
-  ]
+      ["amazon", "https://www.amazon.com/", "amz"],
+      ["graphic", "https://graphicburger.com/", "gb"],
+      ["gutenberg", "https://www.gutenberg.org/", "pg"],
+      ["rainy mood", "https://rainymood.com/", "rm"],
+    ],
+  ],
 ];
